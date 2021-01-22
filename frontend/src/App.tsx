dd3617aececb987f4660c987429e3b265ba3a7c7
Dmytro Orikhovskyi <d>
2021-01-22 21:52:26 +0200

    create components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Nav from './components/Nav'
+import Menu from './components/Menu'
+import Dashboard from './components/Dashboard'
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <div className="container-fluid">
+        <div className="row">
+          <Menu />
+          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+            <Dashboard />
+          </main>
+        </div>
+      </div>
     </div>
-  );
-}
+  )
 
 export default App;

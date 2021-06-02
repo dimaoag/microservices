cbc0cd1e56f7a89e834821fad33f1bca09e1e5c5
Dmytro Orikhovskyi <d>
2021-06-02 21:40:09 +0300

    active link

--- frontend/src/pages/secure/Header.vue
+++ frontend/src/pages/secure/Header.vue
@@ -5,10 +5,10 @@
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li>
-            <router-link to="/" class="nav-link px-2 link-dark" active-class="link-secondary" exact>Dashboard</router-link>
+            <router-link to="/dashboard" class="nav-link px-2" active-class="link-secondary">Dashboard</router-link>
           </li>
           <li>
-            <router-link to="/users" class="nav-link px-2 link-dark" active-class="link-secondary" exact>Users</router-link>
+            <router-link to="/users" class="nav-link px-2" active-class="link-secondary">Users</router-link>
           </li>
         </ul>
 
